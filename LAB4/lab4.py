ff88be1609b94d139b6e0150fe94cb1968f5d288
Rachel Brandes <rbrandes@umich.edu>
2017-09-27 18:57:23 -0400

    completed lab 4

--- LAB4/lab4.py
+++ LAB4/lab4.py
@@ -6,3 +6,7 @@ f = f.readlines()
 for line in f:
 	if re.search("From", line):
 		print (line)
+		numbers= re.findall("[0-9]+",line)
+		print (numbers)
+		names= re.findall("(\S+)@", line)[0]
+		print (names)
